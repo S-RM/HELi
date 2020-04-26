30ffc16c7708378e35c73a544c1494b4a1fe3dfd
Sankgreall <jpjackson@protonmail.com>
2020-04-26 19:15:06 +1200

    Update elastic.py

--- lib/elastic.py
+++ lib/elastic.py
@@ -40,12 +40,6 @@ def thread_worker(item_queue, GlobalRecordCount, GlobalPercentageComplete, Globa
         try:
             item = item_queue.get() # Try for up to 1 sec to get data, else give up
 
-            try:
-                if item['data']['token'] is None:
-                    item['data']['token'] = ""
-            except KeyError:
-                item['data']['token'] = ""
-
             if item == "STOP":
                 break
 
