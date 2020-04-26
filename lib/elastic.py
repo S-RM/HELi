bc902d8e2a49221af67fc200e903a4a90b8b28e0
Sankgreall <jpjackson@protonmail.com>
2020-04-26 18:54:49 +1200

    Update elastic.py

--- lib/elastic.py
+++ lib/elastic.py
@@ -89,6 +89,8 @@ def postToElastic(events, index, nodes, token=""):
         try:
             # Post to current node
             results = requests.post("http://" + nodes[currentNode] + "/" + index + "/doc/_bulk", data=events, headers=headers)
+            print results
+            exit()
             if results.status_code == 200:
                 success = True
 
