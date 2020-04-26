db9db75cd2f686e5da63ffdbfda88258af632c54
Sankgreall <jpjackson@protonmail.com>
2020-04-26 13:30:12 +1200

    pypy experimentation

--- lib/evtxengine.py
+++ lib/evtxengine.py
@@ -92,7 +92,12 @@ def count_records_in_batch(queue, file_path, receive_queue):
         
         # Check for up to one second if we can grab some data, else exit
         try:
-            chunk_offset = queue.get(False)
+            if not queue.empty():
+                chunk_offset = queue.get(False)
+            else:
+                receive_queue.put("STOP")
+                shutdown = True
+                break
             
 
         except queues.Empty:
